31be37a47049648f72546a9a34b79304fd1e60f4
Toan <crisriorock0@gmail.com>
2021-03-23 16:02:50 +0700

    Thay doi sudoku repons

---
+++ main.js
@@ -0,0 +1,511 @@
+//Declare 
+let selectedNum, selectedTile, disableSelect, lives;
+let timeRemaining, timer;
+let stop = false; // variable test lives Remainning and Time lives
+let pen = false; // variable test pen has used
+let solution = [];
+let difficulty;
+let gridSudoku = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+]; 
+
+let grid = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+];
+
+// backtrack find solution sudoku
+function solveSudoku(grid, row, col) {
+    let cell = findUnassignedLocation(grid, row, col);
+    row = cell[0];
+    col = cell[1];
+    if (row == -1) {
+        return true;
+    }
+    let numb = [1,2,3,4,5,6,7,8,9];
+    shuffleArray(numb);
+    for (let numt = 0; numt < 9; numt++) {
+        let num = numb[numt];
+        if ( noConflicts(grid, row, col, num) ) {   
+            grid[row][col] = num;
+
+            if ( solveSudoku(grid, row, col) ) {                
+                return true;
+            }
+            grid[row][col] = 0;
+        }
+    }
+    return false;
+}
+//Shuffle Array 1 - 9
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];  
+    }
+}
+//Find unassigned location
+function findUnassignedLocation(grid, row, col) {   
+    for (; row < 9 ; col = 0, row++)
+    for (; col < 9 ; col++)
+        if (grid[row][col] == 0) 
+            return [row, col];
+    return [-1, -1];
+}
+
+function noConflicts(grid, row, col, num) {
+    return isRowOk(grid, row, num) && isColOk(grid, col, num) && isBoxOk(grid, row, col, num);
+}
+
+function isRowOk(grid, row, num) {
+    for (let col = 0; col < 9; col++)
+        if (grid[row][col] == num)
+            return false;
+
+    return true;
+}
+function isColOk(grid, col, num) {
+    for (let row = 0; row < 9; row++)
+    if (grid[row][col] == num)
+        return false;
+
+    return true;    
+}
+function isBoxOk(grid, row, col, num) {
+    row = Math.floor(row / 3) * 3;
+    col = Math.floor(col / 3) * 3;
+
+    for (let r = 0; r < 3; r++)
+        for (let c = 0; c < 3; c++)
+            if (grid[row + r][col + c] == num)
+                return false;
+
+    return true;
+}
+//easy-5  medium-9 hard- 100
+function deleteElement(grid){
+    let gridDelete = grid;  
+    let num = [0,1,2,3,4,5,6,7,8];    
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < difficulty; j++) {
+            shuffleArray(num);
+            gridDelete[i][num[j]] = 0;
+        }
+    }
+    return gridDelete;
+}
+
+// Above build the solution grid sudoku
+function printGrid(grid) {
+    let res = "";
+
+    for (let i = 0; i < 81; i++) {
+        res += grid[i] + "  ";
+        if((i + 1)%9 ===0 ) res += "\n";        
+    }
+    console.log(res);
+}
+
+
+//Main function for sudoku game
+//Controller
+window.onload = function() {
+    //Run startgame function when button is clicked
+    id("start-btn").addEventListener("click", startGame); 
+    //Add event listener to each number in number container
+    for (let i = 0; i < id("number-container").children.length; i++) {
+        id("number-container").children[i].addEventListener("click", function() {
+            //If selecting is not disabled
+            if(!disableSelect){
+                //If number is already selected
+                if(this.classList.contains("selected")) {
+                    //Then remove selection
+                    this.classList.remove("selected");
+                    selectedNum = null;
+                } else {
+                    //Deselected all other numbers
+                    for (let i = 0; i < 9; i ++) {
+                        id("number-container").children[i].classList.remove("selected"); // 146 != 219
+                    }//if only tile is selected
+                    //Select it and update selectedNum Variable
+                    this.classList.add("selected");
+                    selectedNum = this;
+                    updateMove();
+                }
+            }
+        });
+    }
+}
+
+function startGame() {
+    let board;
+    //Set difficulty for grid
+    if(id("diff-1").checked){
+        difficulty = 5;
+    }else if(id("diff-2").checked) {
+        difficulty = 7;
+    }else
+        difficulty = 18;
+    board = createGrid();
+    //Create board based on difficulty
+    lives = 4;
+    id("lives").textContent = "Lives Remainning: " + (lives-1);
+    //Create board based on difficulty
+    generateBoard(board);
+    //Start the time 
+    startTimer();
+    //Sets theme based on input 
+    if(id("theme-1").checked) {
+        qs("body").classList.remove("dark");
+    } else {
+        qs("body").classList.add("dark");
+    }    
+    //Show number container
+    id("number-container").classList.remove("hidden");  
+    //Show time live 
+    id("time").classList.remove("hidden"); 
+}
+
+function startTimer() {
+    //Sets time start
+    timeRemaining = 1200;
+    id("timer").textContent = timeConversion(timeRemaining);
+    timer = setInterval(function() {
+        if(timeRemaining > 0 && stop === false) timeRemaining--;
+        //If no time Remaining end the game
+        if(timeRemaining === 0) endGame();
+        id("timer").textContent = timeConversion(timeRemaining);        
+    }, 1000);
+}
+
+function timeConversion(time) {
+    let minutes = Math.floor(time/60);
+    if(minutes < 10) minutes = "0" + minutes;
+    let seconds = time % 60;
+    if (seconds < 10) seconds = "0" + seconds;
+    return minutes + ":" + seconds; 
+}
+
+function createGrid(){
+    let board = [];
+    grid = [
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]
+    ];     
+    gridSudoku = [
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]
+    ];
+    solution = [];
+    //Create solution 
+    solveSudoku(grid,0,0);
+    //Create grid
+    for(let i = 0; i < 9; i++){
+        for(let j = 0; j < 9; j++){
+            //Change Array 2 > 1
+            solution.push(grid[i][j]);
+        }
+    }
+//    printGrid(solution);
+    gridSudoku = deleteElement(grid);
+    for(let i = 0; i < 9; i++){
+        for(let j = 0; j < 9; j++){
+            //Change Array 2 > 1
+            board.push(grid[i][j]);
+        }
+    }
+    return board;
+}
+function generateBoard(board) {
+    //Clear previous board
+    clearPrevious();
+    //Let used to increment tile ids
+    let idCount = 0;
+    //Create 81 tiles
+    for (let i = 0; i < 81; i++){
+        //Create a new paragraph elements
+        let tile = document.createElement("p");
+        
+        //If tile has number == 0
+        if (board[i] != 0) {
+            //Set tile text to correct number
+            tile.textContent = board[i];
+        } else {
+            //Add click event listener to tile
+            tile.addEventListener("click", function click() {
+                //If selecting is not disabled
+                if(!disableSelect) {
+                    //If the tile is already selected
+                    if(tile.classList.contains("selected")) {
+                        //Then remove selection
+                        tile.classList.remove("selected");
+                        selectedTile = null;
+                    } else {
+                        //Deselect all other tiles
+                        for(let i = 0; i < 81; i++) {
+                            qsa(".tile")[i].classList.remove("selected"); // 219 != 146
+                        }
+                        // Add selection and update variable
+                        tile.classList.add("selected");
+                        //Delete event when check true
+                        // if(pen === true && solution[tile.id] == tile.textContent) {
+                        //     if(solution[tile.id] == tile.textContent && pen === false) tile.removeEventListener("click", click()); 
+                        // }                        
+                        selectedTile = tile;                                                                                          
+                        updateMove(tile);
+                        try {
+                            let font = selectedTile.style.fontSize;                  
+                            if(tile.textContent !== "" && font === "35pt" || font === "25pt") tile.removeEventListener("click", click());
+                        } catch (error) {
+                            
+                        }
+                    }
+                }
+            });
+        }   
+        // Assign tile id
+        tile.id = idCount;
+        //Increment for next tile
+        idCount ++;
+        //Add tile class to all tiles
+        tile.classList.add("tile");
+        //Add border
+        if ((tile.id > 17 && tile.id < 27) || (tile.id > 44 && tile.id < 54)){
+            tile.classList.add("bottomBorder");
+        }
+        if((tile.id + 1) % 9 == 3 || (tile.id + 1) % 9 == 6) {
+            tile.classList.add("rightBorder");
+        }
+        if ((tile.id >= 0 && tile.id < 9)){
+            tile.classList.add("topBorder");
+        }
+        if (tile.id % 9 === 0){
+            tile.classList.add("leftBorder");
+        }
+        if ((tile.id+1) % 9 === 0){
+            tile.classList.add("rightBorder");
+        }
+        //Add tile to board
+        id("board").appendChild(tile);
+        if ((tile.id >= 72 && tile.id < 81)){
+            tile.classList.add("bottomBorder");
+        }
+    }
+}
+
+function updateMove(tile) {
+    //If a tile and a number is selected
+    if(selectedTile && selectedNum && (Number(selectedNum.textContent) > 0 && Number(selectedNum.textContent) <= 9)) {        
+        deletePen(selectedTile);
+        //If pen === true update class .tile        
+        if(pen === true) {                
+            selectedTile.style.fontSize = "8pt"; 
+            if(!selectedTile.textContent.match(selectedNum.textContent)){
+                selectedTile.textContent += " "+selectedNum.textContent;
+            }
+            
+        }else{
+            //Set the tile to the correct number
+            selectedTile.style.fontSize = "default"; 
+            
+            selectedTile.style.hover = "black";
+            selectedTile.textContent = selectedNum.textContent;
+        }
+        //If the number matches the corresponding number in the solution key
+        if (checkCorrect(selectedTile)) {       
+            
+            //Deselects the tiles
+            selectedTile.classList.remove("selected");
+            selectedNum.classList.remove("selected");
+            //Clear the selected variable
+            selectedNum = null;
+            selectedTile = null;
+            //Check if board is completed
+            if(checkDone()) {
+                endGame();
+            }            
+            //If the number does not match the solution key
+        } else {
+            //Disable selecting new numbers for one second
+            disableSelect = true;
+            //Make the tile turn red
+            selectedTile.classList.add("incorrect");
+            //Run in one second
+            setTimeout(function (){ 
+                //Subtract lives by one
+                lives --;
+                //If no lives left end the game
+                if(lives === 0){
+                    endGame();
+                }else {
+                    //If lives is not equal to zero
+                    //Update lives text
+                    id("lives").textContent = "Lives Remainning: " + (lives-1);
+                    //Renable selecting numbers and tiles
+                    disableSelect = false;
+                }
+                //Restore tile color and remove selected from both
+                selectedTile.classList.remove("incorrect");
+                selectedTile.classList.remove("selected");
+                selectedNum.classList.remove("selected");
+                //Clear the tiles text and clear selected variables
+                selectedTile.textContent = "";
+                selectedTile = null;
+                selectedNum = null;
+            }, 1000);           
+        }
+    }
+}
+
+function endGame() {    
+    //Display win or loss message
+    if(lives === 0 || timeRemaining <= 0) {
+        id("lives").textContent = "Mission Fail!";
+        stop = true;
+    } else {
+        id("lives").textContent = "Won Won!";
+        stop = true;
+    }
+    disableSelect = true;
+}
+
+function checkDone() {
+    let tiles = qsa(".tile");
+    for(let i = 0; i < tiles.length; i++) {
+        if(tiles[i].textContent === "") return false;
+    }
+    return true;
+}
+
+function checkCorrect(tile) {
+    //If tile's number is equal to solution's 
+    if(solution[tile.id] == tile.textContent || pen === true){         
+        return true;
+    } 
+    else return false; 
+}
+
+function clearPrevious() {
+    // Access all of the tiles
+    let tiles = qsa(".tile");        
+    //Remove each tile
+    for(let i = 0; i < tiles.length; i++){
+        tiles[i].remove();
+    }
+    //Deselect any numbers
+    for(let i = 0; i < id("number-container").children.length; i++) {
+        id("number-container").children[i].classList.remove("selected");
+    }
+    //Set stop = false
+    stop = false;
+    //Clear timer
+    clearInterval(timer);
+    //Clear selected variables
+    selectedTile = null;
+    selectedNum = null;
+    disableSelect = false;
+    //Set note write
+    if(!id("number-option").children[0].classList.contains("pen-off")) {
+        id("number-option").children[0].classList.remove("pen-on");
+        id("number-option").children[0].classList.add("pen-off");   
+    }
+    // set font
+    
+}
+
+// helper Function
+
+function createSolution() {
+    if(confirm("There is only one thing that makes a dream impossible to achieve: the fear of failure.")){
+        for(let i = 0; i < 81; i++) {
+            if(qsa(".tile")[i].textContent == '' || qsa(".tile")[i].style.fontSize == "8pt"){
+                qsa(".tile")[i].style.fontSize = 'default';
+                qsa(".tile")[i].style.color = "red";
+                qsa(".tile")[i].textContent = solution[i];
+            }
+        }  
+        stop = true;
+        timeRemaining = 0;
+        //id("lives").textContent = "Mission Fail!";
+        endGame();
+    }
+
+}
+
+function deletePen(selectedTile){
+    id("number-option").children[1].addEventListener("click", function(){
+        if(selectedTile.classList.contains("selected")) {
+            selectedTile.textContent = "";
+            selectedTile.classList.remove("selected");
+        }
+    });
+
+}
+
+
+function noteWrite() {
+    if(id("number-option").children[0].classList.contains("pen-off")) {
+        id("number-option").children[0].classList.add("pen-on");
+        id("number-option").children[0].classList.remove("pen-off");   
+        pen = true;     
+    } else {
+        id("number-option").children[0].classList.remove("pen-on");
+        id("number-option").children[0].classList.add("pen-off");  
+        pen = false;
+    }
+
+}
+
+function playPause() {
+    if(id("play-pause").children[0].classList.contains("btn-play")){
+        id("play-pause").children[0].classList.remove("btn-play");
+        id("play-pause").children[0].classList.add("btn-pause");        
+        for(let i = 0; i < id("board").children.length; i++) {
+            id("board").children[i].classList.add("hidden");
+        }
+        stop = true;
+    } else {
+        id("play-pause").children[0].classList.add("btn-play");
+        for(let i = 0; i < id("board").children.length; i++) {
+            id("board").children[i].classList.remove("hidden");
+        }
+        stop = false;
+    } 
+
+}
+
+
+let id = id => document.getElementById(id);
+
+let qs = (selector) => document.querySelector(selector);
+
+let qsa = (selector) => document.querySelectorAll(selector);
